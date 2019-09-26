772094956425bf9ff25460e4b41a5c4b916c8dd4
Danish Faruqi <dfaruqi@records.nyc.gov>
2019-09-26 16:32:51 -0400

    changed settings

--- landing_page/__init__.py
+++ landing_page/__init__.py
@@ -2,8 +2,12 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
-app.config.from_object('landing_page.default_settings')
-app.config.from_envvar('LANDING_PAGE_SETTINGS')
+
+if os.getenv('LANDING_PAGE_SETTINGS', None):
+    app.config.from_envvar('LANDING_PAGE_SETTINGS')
+else:
+    app.config.from_object('landing_page.default_settings')
+
 
 if not app.debug:
     import logging
